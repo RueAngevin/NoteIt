4637b64ed8b83ebab1f07429b7b60b4a29c6b529
RueAngevin <rueangevin@gmail.com>
2024-02-02 20:16:33 +0530

    File format change

--- src/componenets/Note.jsx
+++ src/componenets/Note.jsx
@@ -3,8 +3,18 @@ import React from 'react';
 const Note = () => {
     return (
         <>
-            <div className='w-full h-full bg-slate-300 border-2 border-slate-400 rounded-lg'>
-                
+            <div className='w-full h-full p-5 flex flex-col bg-slate-300 border-2 border-slate-400 rounded-lg'>
+
+                <div>
+                    <input className='w-full h-[5rem] p-2 text-6xl bg-transparent outline-none' />
+
+
+                </div>
+                    <hr className='border-black border-[1.5px]' />
+
+                <div className='w-full h-full'>
+                    <textarea className='w-full h-full p-2 text-6xl bg-transparent outline-none resize-none'></textarea>
+                </div>
             </div>
         </>
     );
